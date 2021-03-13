2cd788f27b99ac3b4694c3cc3c6d4c7d44423278
elu2 <60558250+elu2@users.noreply.github.com>
2021-03-13 15:29:34 -0700

    Bug fixes for increased throughput.

--- LabelNodes.py
+++ LabelNodes.py
@@ -7,14 +7,18 @@ import csv
 
 file_name = "nodes_table_all.csv"
 node_labels = []
+reader_s = []
 with open(file_name, "r") as nodes_table:
     reader = nodes_table.readlines()
     for row in reader[1:]:
+        reader_s.append(row.strip('"')[:-2])
         node_labels.append(row.split("::")[0][1:])
-        
-nodes_table = pd.read_csv(file_name, encoding='latin-1')
-nodes_table["label"] = nodes_table["Id"]
-del nodes_table['label']
-nodes_table["Label"] = node_labels 
 
-nodes_table.to_csv("nodes_table_all_labelled.csv")
+labelled_dict = {
+    "Id" : reader_s,
+    "Label": node_labels
+}
+
+labelled_df = pd.DataFrame.from_dict(labelled_dict)
+
+labelled_df.to_csv("nodes_table_all_labelled.csv")
