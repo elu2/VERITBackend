392f361821c53ff30788b1b2c61d5c50b19cc27d
elu2 <60558250+elu2@users.noreply.github.com>
2021-04-10 15:29:50 -0700

    Imporoved output readability.

--- DictChecker.py
+++ DictChecker.py
@@ -15,15 +15,21 @@ queries_id = []
 with open("query_list.txt", 'r', encoding="UTF-8") as query_list:
     queries_id = query_list.readlines()
 
+print(f"Reading {len(queries_id)} queries.")
+
 queries_id = [line.strip() for line in queries_id]
 
 all_in = True
+not_in_counter = 0
 for query in queries_id:
     if query not in all_species:
         print(f"{query} not in network.")
         all_in = False
+        not_in_counter += 1
     else:
         continue
 
 if all_in == True:
     print("All queries in network.")
+else:
+    print(f"{not_in_counter}/{len(queries_id)} species out of network.")
