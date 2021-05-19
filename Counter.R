acc27ff9cd61e9e09a4d3ab9d5cb3989902bd2e0
elu2 <60558250+elu2@users.noreply.github.com>
2021-05-19 16:20:38 -0700

    Create Counter.R
    Counts NC and Act pairs.

---
+++ Counter.R
@@ -0,0 +1,9 @@
+setwd("/xdisk/guangyao/REACH2/REACHVisualization/")
+library(sqldf)
+
+uncounted <- read.csv("AllActNC.csv")
+counted_df <- sqldf("SELECT INPUT, OUTPUT, CONTROLLER, EVENT_LABEL, COUNT(*) AS COUNTER, SEEN_IN FROM uncounted GROUP BY OUTPUT_ID, CONTROLLER_ID, EVENT_LABEL ORDER BY COUNTER DESC")
+
+# Removes anomalous rows
+cleaned <- sqldf("SELECT * FROM counted_df WHERE SEEN_IN != ''")
+write.csv(cleaned, file="Counted.csv", row.names=FALSE)
