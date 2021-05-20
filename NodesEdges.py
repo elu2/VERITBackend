cb3c1e8585c3028c3f0d42830f3da60c450ec8ee
elu2 <60558250+elu2@users.noreply.github.com>
2021-05-19 18:36:56 -0700

    Update and rename Nodes.py to NodesEdges.py
    Replaces NodesEdges.py entirely.

---
+++ NodesEdges.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+tanh_df = pd.read_csv("Tanh.csv")
+
+inputs = tanh_df[["INPUT"]].rename(columns={"INPUT": "Id"})
+controllers = tanh_df[["CONTROLLER"]].rename(columns={"CONTROLLER": "Id"})
+
+nodes = inputs.append(controllers).drop_duplicates(inplace=False)
+
+nodes.to_csv("nodes_table_all.csv", index=False)
+
+# -------------------------------------------------------------------------------
+
+rename_dict = {
+    "CONTROLLER": "source",
+    "CONT_ID": "source_id",
+    "INPUT": "target",
+    "INPUT_ID": "target_id",
+    "EDGE": "color_col",
+    "TOTAL": "thickness"
+              }
+
+tanh_df = tanh_df.rename(columns = rename_dict)
+tanh_df = tanh_df[["source","source_id","target","target_id","color_col","thickness"]]
+
+tanh_df.to_csv("edges_table.csv", index=False)
