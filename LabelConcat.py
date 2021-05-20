4ef2452fab20e2f3eedbc9ee5fb6f9bb9504de77
elu2 <60558250+elu2@users.noreply.github.com>
2021-05-19 17:08:48 -0700

    Create LabelConcat.py
    Replaces ActLabelConcat and NCLabelConcat.

---
+++ LabelConcat.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+df = pd.read_csv("Counted.csv", encoding="UTF-8")
+
+redone_dict = {
+    "OUTPUT" : [],
+    "CONTROLLER" : []
+}
+
+outputs = df["OUTPUT"]
+controllers = df["CONTROLLER"]
+
+for i in range(len(outputs)):
+    if "." in str(outputs[i]):
+        redone_dict["OUTPUT"].append(outputs[i][:outputs[i].index(".")])
+    else:
+        redone_dict["OUTPUT"].append(outputs[i])
+
+for i in range(len(controllers)):
+    if "." in str(controllers[i]):
+        redone_dict["CONTROLLER"].append(controllers[i][:controllers[i].index(".")])
+    else:
+        redone_dict["CONTROLLER"].append(controllers[i])
+
+df2 = pd.DataFrame.from_dict(redone_dict)
+
+df["OUTPUT"] = df2["OUTPUT"]
+df["CONTROLLER"] = df2["CONTROLLER"]
+
+numeric_labels = {"NUM_LABEL": []}
+for label in df.EVENT_LABEL:
+    if label == "Activation (Negative)":
+        numeric_labels["NUM_LABEL"].append(-1)
+    elif label == "Activation (Positive)":
+        numeric_labels["NUM_LABEL"].append(1)
+    else:
+        numeric_labels["NUM_LABEL"].append(0)
+
+df = pd.concat([df, pd.DataFrame(numeric_labels)], axis=1)
+
+df.to_csv("Lab_df.csv", index=False)
