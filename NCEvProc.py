76263262881c7efc27333e80482bdb11740ed475
elu2 <60558250+elu2@users.noreply.github.com>
2021-05-19 20:33:01 -0700

    Fixed encoding bug.

--- NCEvProc.py
+++ NCEvProc.py
@@ -10,7 +10,7 @@ base_path = "/xdisk/guangyao/REACH2/REACHVisualization/"
 
 
 def noneConcat(file):
-    df = pd.read_csv(file, sep='\t', header=0)
+    df = pd.read_csv(file, sep='\t', header=0, quoting=csv.QUOTE_NONE, encoding='utf-8')
     ev_df = df.iloc[:, [0, 1, 2, 3, 4, 17, 18]]
     none_df = ev_df.query("CONTROLLER=='NONE'").iloc[:, [3, 4]]
     joined = pd.merge(ev_df, none_df, left_on="INPUT", right_on="EVENT ID")
@@ -94,7 +94,7 @@ def all_NC_concat(base_path):
         
 # Restructures dataframe to be compatible with other scripts
 def conformity(csv_path):
-    df = pd.read_csv(csv_path, sep=',', header=0).iloc[:, 1:]
+    df = pd.read_csv(csv_path, sep=',', header=0, quoting=csv.QUOTE_NONE, encoding='utf-8').iloc[:, 1:]
     df = df[["INPUT", "OUTPUT", "CONTROLLER", "EVENT ID_x", "EVENT_LABEL", "EVIDENCE", "SEEN IN"]]
     colnames = ["INPUT", "OUTPUT", "CONTROLLER", "EVENT_ID", "EVENT_LABEL", "EVIDENCE", "SEEN_IN"]
     df.columns=colnames
