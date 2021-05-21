986b082a48fdaa0164c8e55e285bc722d8c4eede
elu2 <60558250+elu2@users.noreply.github.com>
2021-05-20 22:06:52 -0700

    IDs in Counted.csv now.

--- Counter.R
+++ Counter.R
@@ -2,7 +2,7 @@ setwd("/xdisk/guangyao/REACH2/REACHVisualization/")
 library(sqldf)
 
 uncounted <- read.csv("AllActNC.csv")
-counted_df <- sqldf("SELECT INPUT, OUTPUT, CONTROLLER, EVENT_LABEL, COUNT(*) AS COUNTER, SEEN_IN FROM uncounted GROUP BY OUTPUT_ID, CONTROLLER_ID, EVENT_LABEL ORDER BY COUNTER DESC")
+counted_df <- sqldf("SELECT INPUT, OUTPUT, CONTROLLER, EVENT_LABEL, COUNT(*) AS COUNTER, SEEN_IN, OUTPUT_ID, CONTROLLER_ID FROM uncounted GROUP BY OUTPUT_ID, CONTROLLER_ID, EVENT_LABEL ORDER BY COUNTER DESC")
 
 # Removes anomalous rows
 cleaned <- sqldf("SELECT * FROM counted_df WHERE SEEN_IN != ''")
