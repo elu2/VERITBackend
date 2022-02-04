fee5ead34392ed95b3fd8d3713b2af63bcbc1666
elu2 <60558250+elu2@users.noreply.github.com>
2022-02-04 16:55:19 -0700

    Remove UAZ IDs.

--- RatExtermination.py
+++ RatExtermination.py
@@ -6,17 +6,20 @@ import pandas as pd
 
 # rat_df: dataframe to prune from
 # pest_str: string to filter out
-def exterminator(rat_df, pest_str):
-    rat_df = rat_df[~rat_df.CONTROLLER.str.contains(pest_str)]
-    exterminated = rat_df[~rat_df.OUTPUT.str.contains(pest_str)]
-    return exterminated
+def exterminator(rat_df, pest_strs):
+    for tstr in pest_strs:
+        rat_df = rat_df[~rat_df.CONTROLLER.str.contains(tstr)]
+        rat_df = rat_df[~rat_df.OUTPUT.str.contains(tstr)]
+    return rat_df
 
 
-df1 = pd.read_csv("Counted.csv", encoding="utf-8")
-df2 = pd.read_csv("AllActNC.csv", encoding="utf-8")
+df1 = pd.read_csv("Counted.csv", encoding="utf-8").astype(str)
+df2 = pd.read_csv("AllActNC.csv", encoding="utf-8").astype(str)
 
-df1_ext = exterminator(df1, "mice:")
-df2_ext = exterminator(df2, "mice:")
+pest_strs = [":uaz:", "mice:"]
+
+df1_ext = exterminator(df1, pest_strs)
+df2_ext = exterminator(df2, pest_strs)
 
 df1_ext.to_csv("Counted.csv")
 df2_ext.to_csv("AllActNC.csv")
