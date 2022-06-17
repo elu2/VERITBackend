612b35c57e7f16ed275d78f639cdaaa05083bed0
elu2 <60558250+elu2@users.noreply.github.com>
2022-06-17 16:28:10 -0700

    Parallelized

--- ActEvProc.py
+++ ActEvProc.py
@@ -3,8 +3,9 @@ import os
 import time
 import csv
 import datetime
+from joblib import Parallel, delayed
 
-base_path = "/xdisk/guangyao/REACH2/REACHVisualization/"
+base_path = "/xdisk/guangyao/elu2/REACHVisualization/"
 
 
 def truncator(path):
@@ -13,23 +14,11 @@ def truncator(path):
     return ev_df
 
 
-def all_Act_concat(base_path):
-    paper_path = base_path + "papers_as_tsv/"
-    directory = os.fsencode(paper_path)
-
-    # Initialized housekeeping values
-    counter = 0
-    new_ref = time.time()
-    time_diffs = []
-
-    # Initialize csv file with column names
-    column_names = ["INPUT", "OUTPUT", "CONTROLLER", "EVENT_ID", "EVENT_LABEL", "EVIDENCE", "SEEN_IN"]
-    base_df = pd.DataFrame(columns = column_names)
-    base_df.to_csv('AllAct.csv', mode='w', header=True)
+def all_Act_concat(paper_list):
     base_df = pd.DataFrame()
 
     # Loop through papers directory
-    for file in os.listdir(directory):
+    for file in paper_list:
         filename = os.fsdecode(file)
         file_path = paper_path + filename
         
@@ -76,5 +65,16 @@ def cleaner(csv_path):
     cleaned.to_csv("AllAct.csv", mode='w', header=True, index=False)
     
     
-all_Act_concat(base_path)
+# Initialize csv file with column names
+column_names = ["INPUT", "OUTPUT", "CONTROLLER", "EVENT_ID", "EVENT_LABEL", "EVIDENCE", "SEEN_IN"]
+base_df = pd.DataFrame(columns = column_names)
+base_df.to_csv('AllAct.csv', mode='w', header=True)
+
+paper_path = base_path + "papers_as_tsv/"
+directory = os.fsencode(paper_path)
+all_files = os.listdir(directory)
+file_chunks = np.array_split(np.array(all_files), 40)
+
+Parallel(n_jobs=-1)(delayed(all_Act_concat)(paper_list) for paper_list in file_chunks)
+
 cleaner(base_path + "AllAct.csv")
