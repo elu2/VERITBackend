e5c92c5f3915a84f5da9eb23be6892dea36514b1
elu2 <60558250+elu2@users.noreply.github.com>
2022-06-20 01:34:48 -0700

    Modernization

--- NCEvProc.py
+++ NCEvProc.py
@@ -1,107 +1,95 @@
-# Processes all NONE CONTROLLERs and concatenates into large csv file.
-
 import pandas as pd
-import os
-import time
-import csv
-import datetime
-from joblib import Parallel, delayed
 import numpy as np
-
-# Path to directory before papers_as_tsv/
-base_path = "/xdisk/guangyao/elu2/REACHVisualization/"
-
-
-def noneConcat(file):
-    df = pd.read_csv(file, sep='\t', header=0, quoting=csv.QUOTE_NONE, encoding='utf-8').astype(str)
-    ev_df = df.iloc[:, [0, 1, 2, 3, 4, 17, 18]]
-    none_df = ev_df.query("CONTROLLER=='NONE'").iloc[:, [3, 4]]
-    joined = pd.merge(ev_df, none_df, left_on="INPUT", right_on="EVENT ID")
-
-    joined_dict = joined.to_dict()
-    joined_dict["EVENT_LABEL"] = []
-
-    for i in range(len(joined_dict["CONTROLLER"])):
-        if joined_dict["EVENT LABEL_x"][i] == "Regulation (Negative)":
-            if joined_dict["EVENT LABEL_y"][i] == "Amount":
-                joined_dict["EVENT_LABEL"].append("Activation (Positive)")
-            elif joined_dict["EVENT LABEL_y"][i] == "Transcription":
-                joined_dict["EVENT_LABEL"].append("Activation (Negative)")
-            elif joined_dict["EVENT LABEL_y"][i] == "Ubiquitination":
-                joined_dict["EVENT_LABEL"].append("Activation (Positive)")
-            else:
-                joined_dict["EVENT_LABEL"].append("Inconclusive")
+import os
+from joblib import Parallel, delayed
+import csv
 
 
-        elif joined_dict["EVENT LABEL_x"][i] == "Regulation (Positive)":
-            if joined_dict["EVENT LABEL_y"][i] == "Amount":
-                joined_dict["EVENT_LABEL"].append("Activation (Negative)")
-            elif joined_dict["EVENT LABEL_y"][i] == "Transcription":
-                joined_dict["EVENT_LABEL"].append("Activation (Positive)")
-            elif joined_dict["EVENT LABEL_y"][i] == "Ubiquitination":
-                joined_dict["EVENT_LABEL"].append("Activation (Negative)")
-            else:
-                joined_dict["EVENT_LABEL"].append("Inconclusive")
-
-    joined_dict["EVENT_LABEL"] = pd.Series(joined_dict["EVENT_LABEL"])
-    joined_ev_df = pd.DataFrame(joined_dict)
-    joined_ev_df = joined_ev_df.drop_duplicates(subset=["OUTPUT", "CONTROLLER", "SEEN IN", "EVENT_LABEL"]).reset_index(drop=True)
-    trunc_df = joined_ev_df.iloc[:, [0, 1, 2, 3, 5, 6, 9]]
-
-    return trunc_df
+def truncator(path):
+    df = pd.read_csv(path, sep='\t', header=0, quoting=csv.QUOTE_NONE, encoding='utf-8', dtype=str).astype(str)
+    ev_df = df[spec_cols]
+    return ev_df
 
 
-def all_NC_concat(paper_list):
-    # Initialize csv file with column names
-    base_df = pd.DataFrame()
-    counter = 0
+def net_event(df):
+    nc_df = df[df["EVENT LABEL"].str.contains("Activation") == False]
+    id_df = nc_df[nc_df["CONTROLLER"] == "NONE"][["EVENT ID", "EVENT LABEL"]]
+    nc_df = nc_df.merge(id_df, left_on="INPUT", right_on="EVENT ID", suffixes=("", " INIT"))
 
+    # Define maps to replace values with
+    map_dict_el = {"Regulation (Negative)": 1, "Regulation (Positive)": -1}
+    map_dict_eli = {"Transcription": 1, "Amount": 1, "DecreaseAmount": -1, "Ubiquitination": -1}
+    final_map = {1: "Activation (Positive)", -1: "Activation (Negative)", 0: "Inconclusive"}
+    
+    # Only some events are hardcoded. The others are treated as inconclusive
+    all_events = list(set(nc_df["EVENT LABEL INIT"]))
+    for event in all_events:
+        if event not in map_dict_eli.keys():
+            map_dict_eli[event] = 0
+    all_events = list(set(nc_df["EVENT LABEL"]))
+    for event in all_events:
+        if event not in map_dict_el.keys():
+            map_dict_eli[event] = 0
+
+    # Replace events with numerical "directions"
+    nc_df = nc_df.replace({"EVENT LABEL": map_dict_el})
+    nc_df = nc_df.replace({"EVENT LABEL INIT": map_dict_eli})
+
+    # Get net direction of the paired interactions
+    nc_df["EVENT LABEL"] = nc_df["EVENT LABEL"] * nc_df["EVENT LABEL INIT"]
+
+    # finally replace values
+    nc_df = nc_df.replace({"EVENT LABEL": final_map})
+    nc_df = nc_df.drop(["EVENT ID INIT", "EVENT LABEL INIT"], axis=1)
+    
+    return nc_df
+
+
+def post_proc(df):
+    # Remove NONE controllers
+    df = df[df.CONTROLLER != "NONE"]
+    # Remove regulation event labels
+    df = df[df["EVENT LABEL"].str.contains("Regulation", na=False) == False]
+    # Remove :uaz: rows
+    drop_rows = (df["INPUT"].str.contains(":uaz:") == False) * (df["OUTPUT"].str.contains(":uaz:") == False) * (df["CONTROLLER"].str.contains(":uaz:") == False)
+    # Remove two double colon species
+    drop_rows = drop_rows * (df["OUTPUT"].str.contains('{') == False)
+
+    df = df[drop_rows]
+
+    return df
+
+
+def concat_papers(paper_list, paper_path="./papers_as_tsv/"):
     # Loop through papers directory
     for file in paper_list:
-        filename = os.fsdecode(file)
-        file_path = paper_path + filename
-        file_df = noneConcat(file_path)
+        file_path = paper_path + file
+        file_df = truncator(file_path)
+        file_df = net_event(file_df)
+        print(file)
 
-        base_df = base_df.append(file_df, ignore_index=True)
+        file_df.to_csv('AllNC.csv', mode='a', header=False, index=False)
 
-        counter += 1
-        if counter % 1000 == 0:
-            base_df.to_csv('AllNC.csv', mode='a', header=False)
-            base_df = pd.DataFrame()
-
-    # Unload last iteration of <1000 papers
-    base_df.to_csv('AllNC.csv', mode='a', header=False, index=False)
+    return None
 
 
-# Restructures dataframe to be compatible with other scripts
-def conformity(csv_path):
-    df = pd.read_csv(csv_path, sep=',', header=0, error_bad_lines=False, encoding='utf-8').iloc[:, 1:]
-    df = df[["INPUT", "OUTPUT", "CONTROLLER", "EVENT ID_x", "EVENT_LABEL", "EVIDENCE", "SEEN IN"]]
-    colnames = ["INPUT", "OUTPUT", "CONTROLLER", "EVENT_ID", "EVENT_LABEL", "EVIDENCE", "SEEN_IN"]
-    df.columns=colnames
+if __name__ == "__main__":
+    # Columns to concat on
+    spec_cols = ["INPUT", "OUTPUT", "CONTROLLER", "EVENT ID", "EVENT LABEL", "EVIDENCE", "SEEN IN"]
 
-    cleaned = df[~df.INPUT.str.contains("uaz", na=False)]
-    cleaned = cleaned[~cleaned.OUTPUT.str.contains("uaz", na=False)]
-    cleaned = cleaned[~cleaned.CONTROLLER.str.contains("uaz", na=False)]
-    cleaned = cleaned[~cleaned.INPUT.str.contains("uaz", na=False)]
-    cleaned = cleaned[~cleaned.OUTPUT.str.contains("nan", na=False)]
-    cleaned = cleaned[~cleaned.CONTROLLER.str.contains("nan", na=False)]
-    cleaned = cleaned[~cleaned.OUTPUT.str.contains("{", na=False)]
-    cleaned = cleaned.dropna()
-    cleaned = cleaned.reset_index(drop=True)
+    # Initialize empty file to append to
+    init_df = pd.DataFrame(columns=spec_cols)
+    if not os.path.exists("AllNC.csv"):
+        init_df.to_csv("AllNC.csv", index=False)
 
-    cleaned.to_csv('AllNC.csv', mode='w', header=True, index=False)
+    # get paper paths and chunk for parallelization
+    paper_path = "./papers_as_tsv/"
+    all_files = [x for x in os.listdir(paper_path) if "PMC" in x]
+    file_chunks = np.array_split(np.array(all_files), 40)
 
-
-column_names = ["INPUT", "OUTPUT", "CONTROLLER", "EVENT ID_x", "EVIDENCE", "SEEN IN", "EVENT_LABEL"]
-base_df = pd.DataFrame(columns = column_names)
-base_df.to_csv('AllNC.csv', mode='w', header=True)
-
-paper_path = base_path + "papers_as_tsv/"
-directory = os.fsencode(paper_path)
-all_files = os.listdir(directory)
-file_chunks = np.array_split(np.array(all_files), 40)
-
-Parallel(n_jobs=-1)(delayed(all_NC_concat)(paper_list) for paper_list in file_chunks)
-
-conformity(base_path + "AllNC.csv")
+    Parallel(n_jobs=1)(delayed(concat_papers)(paper_list) for paper_list in file_chunks)
+    
+    # post-processing of files
+    aa_df = pd.read_csv("AllNC.csv", encoding='utf-8')
+    aa_df = post_proc(aa_df)
+    aa_df.to_csv("AllNC.csv", index=False)
