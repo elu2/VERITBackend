86e0a23f4b43674bbf03c121c4663d03519de2cb
elu2 <60558250+elu2@users.noreply.github.com>
2022-06-21 12:50:21 -0700

    New file names

--- pickler.py
+++ pickler.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import pickle
 
-evidence_pd = pd.read_csv("evidence.csv", encoding="utf-8")
-nodes_pd = pd.read_csv("nodes_table_all_labelled.csv", encoding="utf-8")
-edges_pd = pd.read_csv("edges_table.csv", encoding="utf-8")
+evidence = pd.read_csv("evidence.csv", encoding="utf-8")
+nodes = pd.read_csv("nodes.csv", encoding="utf-8")
+edges = pd.read_csv("edges.csv", encoding="utf-8")
 
-pickle.dump(evidence_pd, open("evidence.pkl", "wb"))
-pickle.dump(nodes_pd, open("nodes_table_all_labelled.pkl", "wb"))
-pickle.dump(edges_pd, open("edges_table.pkl", "wb"))
+evidence.to_pickle("evidence.pkl")
+nodes.to_pickle("nodes.pkl")
+edges.to_pickle("edges.pkl")
