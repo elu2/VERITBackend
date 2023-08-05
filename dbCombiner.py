e8708bd4224e894e407bae8d1f83d499a9168843
Eric Lu <ericjlu2@gmail.com>
2023-08-05 11:29:19 -0700

    Rename, debulk

--- dbCombiner.py
+++ dbCombiner.py
@@ -2,13 +2,13 @@ import pandas as pd
 import pickle
 
 # Read in pubchem db file with new col names
-pubchem_df = pd.read_csv("./databases/pubchemdb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
-chebi_df = pd.read_csv("./databases/chebidb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
-proteins0f_df = pd.read_csv("./databases/proteins0f.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
-proteinsgp_df = pd.read_csv("./databases/proteinsgp.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
-proteinsqz_df = pd.read_csv("./databases/proteinsqz.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
-mesh_df = pd.read_csv("./databases/meshdb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
-gobioproc_df = pd.read_csv("./databases/gobioproc.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop([2, 3], axis=1).drop_duplicates().reset_index(drop=True)
+pubchem_df = pd.read_csv("./knowledgebases/pubchemdb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
+chebi_df = pd.read_csv("./knowledgebases/chebidb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
+proteins0f_df = pd.read_csv("./knowledgebases/proteins0f.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
+proteinsgp_df = pd.read_csv("./knowledgebases/proteinsgp.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
+proteinsqz_df = pd.read_csv("./knowledgebases/proteinsqz.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
+mesh_df = pd.read_csv("./knowledgebases/meshdb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
+gobioproc_df = pd.read_csv("./knowledgebases/gobioproc.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop([2, 3], axis=1).drop_duplicates().reset_index(drop=True)
 
 
 # Adds pubchem tag to ids
@@ -35,4 +35,4 @@ full_df = full_df.append(proteinsqz_df).reset_index(drop=True)
 # Turns all names into lower case
 full_df["name"] = full_df.name.apply(lambda a: str(a).lower())
 
-pickle.dump(full_df, open("./databases/combinedDBs.pkl", "wb"))
+pickle.dump(full_df, open("./knowledgebases/combinedDBs.pkl", "wb"))
--- pickler.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-evidence = pd.read_csv("evidence.csv", encoding="utf-8")
-nodes = pd.read_csv("nodes.csv", encoding="utf-8")
-edges = pd.read_csv("edges.csv", encoding="utf-8")
-
-evidence.to_pickle("evidence.pkl")
-nodes.to_pickle("nodes.pkl")
-edges.to_pickle("edges.pkl")
